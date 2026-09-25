ad6e40a862e0f9f5141701c28b91cc2dcc3f612b
vinicius-oliveira <vinicius.olvrdev@gmail.com>
2026-09-25 14:35:07 -0300

    analisys and graphics

--- src/analisys.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import geopandas as gpd
-
-path = '/home/vinicius/projects/projects/gee-data-pipeline/saidas/subbacias_corrigidas_com_gcn.geojson'
-
-gdf = gpd.read_file(path)
-
